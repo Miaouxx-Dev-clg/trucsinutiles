afb7050921933396d1071a064c56d09b6f00b76f
Miaouxx-Dev-clg <baptiste.henandez@ik.me>
2026-06-18 12:15:28 +0200

    Create maitre.bat

---
+++ maitre.bat
@@ -0,0 +1,19 @@
+@echo off
+title NovaDev85 - Initialisation
+mode con: cols=120 lines=50
+color 0a
+cls
+
+:: Lecture du fichier avec une pause plus longue pour l'effet "machine à écrire"
+:: On passe à 50 millisecondes au lieu de 10
+powershell -Command "Get-Content 'le grand dieu.txt' -ReadCount 1 | ForEach-Object { $_ ; Start-Sleep -Milliseconds 50 }"
+
+:: Ajout de la signature
+echo.
+echo.
+echo ========================================================================
+echo [SYSTEME] Chargement termine.
+echo [SIGNATURE] MiaouxxLab respect eternel a mon maitre Lilian
+echo ========================================================================
+echo.
+pause
